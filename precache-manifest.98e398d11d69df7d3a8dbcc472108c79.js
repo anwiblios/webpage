2d98786bc816be960a74fc6207e7972b34944336
Venkata Sparrow <venkatasparrow@Venkatas-MacBook-Pro.local>
2020-09-28 14:13:48 +0530

    Updates

---
+++ precache-manifest.98e398d11d69df7d3a8dbcc472108c79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957b04311a891844841a71d45855d020",
+    "url": "/webpage/index.html"
+  },
+  {
+    "revision": "3bbb1cd013077526c74c",
+    "url": "/webpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e5fa48601580c44a9a22",
+    "url": "/webpage/static/js/2.5a6fd104.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webpage/static/js/2.5a6fd104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bbb1cd013077526c74c",
+    "url": "/webpage/static/js/main.324f17c0.chunk.js"
+  },
+  {
+    "revision": "634134b9b42f072734eb",
+    "url": "/webpage/static/js/runtime-main.06822ee0.js"
+  }
+]);
